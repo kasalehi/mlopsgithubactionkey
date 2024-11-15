2680d8a1237cacef2a27cfb3560b65f80d495b6a
kasalehi <127022990+kasalehi@users.noreply.github.com>
2024-11-16 11:38:45 +1300

    Update model.py

--- model.py
+++ model.py
@@ -33,14 +33,14 @@ def mae(y_test, y_pred):
   """
   Calculuates mean absolute error between y_test and y_preds.
   """
-  return tf.metrics.mean_absolute_error(y_test, y_pred)
+  return tf.metrics.MeanAbsoluteError(y_test, y_pred)
   
 
 def mse(y_test, y_pred):
   """
   Calculates mean squared error between y_test and y_preds.
   """
-  return tf.metrics.mean_squared_error(y_test, y_pred)
+  return tf.metrics.MeanAbsoluteError(y_test, y_pred)
 
 
 # Check Tensorflow version
